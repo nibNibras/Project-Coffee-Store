7bea34e1ce0e296dc076c260365d8cb25993507a
novaglze <azzahranibrass@gmail.com>
2025-08-01 21:38:40 +0700

    last : about

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible'); // remove this line if you only want fade-in
+    }
+  });
+});
+
+document.querySelectorAll('.fade-section').forEach(section => {
+  observer.observe(section);
+});
