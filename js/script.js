4ddff8989a617db3ab38080668de4d21b60e35b2
novaglze <azzahranibrass@gmail.com>
2025-08-02 22:04:30 +0700

    last : menu

--- js/script.js
+++ js/script.js
@@ -10,17 +10,3 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     navLinks.classList.remove('active');
   });
 });
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    } else {
-      entry.target.classList.remove('visible'); // remove this line if you only want fade-in
-    }
-  });
-});
-
-document.querySelectorAll('.fade-section').forEach(section => {
-  observer.observe(section);
-});
